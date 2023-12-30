d84a6fdd16e270e6859ae0fe794cc088261761a8
fredericklawrence <eerriiccllaaww@gmail.com>
2023-12-30 16:34:56 -0600

    ssh improvement

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 import subprocess
+import os
+
+def generate_rsa_key_pair():
+    """Generates an RSA key pair for SSH authentication."""
+    subprocess.run(["ssh-keygen", "-t", "rsa", "-b", "4096", "-f", "/home/your_user/.ssh/id_rsa", "-q"])
+
+def display_public_key():
+    """Displays the contents of the public SSH key."""
+    with open("/home/your_user/.ssh/id_rsa.pub", "r") as f:
+        public_key = f.read().strip()
+    print("Public SSH key:\n", public_key)
+
+def add_public_key_to_authorized_keys():
+    """Adds the public key to the authorized_keys file."""
+    public_key = open("/home/your_user/.ssh/id_rsa.pub").read()
+    authorized_keys = open("/home/your_user/.ssh/authorized_keys", "a")
+    authorized_keys.write(public_key + "\n")
+    authorized_keys.close()
 
 def update_packages():
     """Updates package lists and upgrades packages."""
@@ -17,11 +35,9 @@ def install_packages(packages):
 def configure_ssh():
     """Configures SSH with secure settings using UFW."""
 
-    # Disable password authentication
-    subprocess.run(["sudo", "sed", "-i", "s/PasswordAuthentication yes/PasswordAuthentication no/", "/etc/ssh/sshd_config"])
-
-    # Disable root login
-    subprocess.run(["sudo", "sed", "-i", "s/#PermitRootLogin yes/PermitRootLogin no/", "/etc/ssh/sshd_config"])
+    # Ensure only public key authentication
+    subprocess.run(["sudo", "sed", "-i", "s/^PasswordAuthentication yes/PasswordAuthentication no/", "/etc/ssh/sshd_config"])
+    subprocess.run(["sudo", "sed", "-i", "s/^PubkeyAuthentication yes/PubkeyAuthentication yes/", "/etc/ssh/sshd_config"])
 
     # Implement rate limiting (using UFW)
     subprocess.run(["sudo", "ufw", "limit", "ssh/tcp", "6/min"])
@@ -47,13 +63,22 @@ def setup_ubuntu_server():
 
     # 3. Create a non-root user with sudo privileges
     if input("Do you want to create a new non-root user? (y/n): ").lower() == "y":
-         username = input("Enter a username for the new user: ")
-         create_user(username)
+        username = input("Enter a username for the new user: ")
+        create_user(username)
+    else:
+        username = input("Enter the username to use for SSH key operations: ")
+
     
     # 4. Configure SSH (optional)
     if input("Do you want to configure SSH? (y/n): ").lower() == "y":
         # Allow public key authentication, disable password authentication, and other security measures
-        configure_ssh()
+        if not os.path.exists("/home/{}/.ssh/id_rsa".format(username)):
+            generate_rsa_key_pair(username)
+
+            add_public_key_to_authorized_keys(username)
+            configure_ssh()
+
+            display_public_key(username)  # Show the generated public key
 
     # Install essential packages (example)
     essential_packages = ["python3", "python3-pip", "git", "ufw", "fail2ban"]
